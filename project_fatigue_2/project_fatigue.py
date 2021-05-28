37776c59bd1fac58728221bd85ac2fbe72d0e2dc
Gisela Rosado Bailón <G.RosadoBailon@student.tudelft.nl>
2021-05-28 12:13:37 +0200

    project_fatigue_fatigue.py: Add test

--- project_fatigue_2/project_fatigue.py
+++ project_fatigue_2/project_fatigue.py
@@ -1,6 +1,13 @@
 import sys
 import numpy as np
 
+def test_range_overlap():
+    assert(range_overlap([(500,10000),(0,2000)]) == (500,2000))
+    assert(range_overlap([(200,1000)]) == (200,1000))
+
+def test_range_no_overlap():
+    assert(range_overlap([(10,1000),(5000,60000)]) == None)
+
 def range_overlap(ranges):
     
     #-------------------------------
@@ -9,6 +16,8 @@ def range_overlap(ranges):
 
     max_left = max(ranges, key= lambda x: x[0])[0]
     min_right = min(ranges, key = lambda x: x[1])[1]
+    
+    #print(max_left, min_right)
     return (max_left, min_right)
 
 if __name__ == '__main__':
@@ -21,7 +30,7 @@ if __name__ == '__main__':
         left_val = np.min(data[:,0])
         right_val = np.max(data[:,0])
         range_val = (left_val,right_val)
-        #print (range_val)
+        #print (file,range_val)
         list_ranges.append(range_val)
 
     result = range_overlap(list_ranges)
